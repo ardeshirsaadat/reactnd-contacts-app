b988345c15c39e4dcbe4defd85e5acb6a2b3885f
ardeshirsaadat <ardeshirsaadat@gmail.com>
2021-01-15 14:58:39 -0500

    Build ListContacts react component

---
+++ src/ListContacts.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react'
+
+class ListContacts extends Component{
+
+  render(){
+    // console.log(this.props.contacts)
+    return(
+    <ol className='contact-list'>
+      {this.props.contacts.map(contact=>(
+        <li key={contact.id} className='contact-list-item'>
+          <div className='contact-avatar'
+          style={{background:`url(${contact.avatarURL})`}}>
+          </div>
+          <div className='contact-details'>
+            <p>{contact.name} </p>
+            <p>{contact.handle}</p>
+          </div>
+          <button className='contact-remove'>
+            remove
+          </button>
+        </li>
+      ))}
+    </ol>)
+
+  }
+  
+
+}
+
+export default ListContacts
